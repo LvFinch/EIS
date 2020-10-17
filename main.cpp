f5a09cc3efadbabbc2b8e4b54a8b6a78438b86cf
Akko <33564554+LvFinch@users.noreply.github.com>
2020-10-17 15:51:03 +0800

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-09-06 16:05:05
+ * @LastEditTime: 2020-09-06 16:14:20
+ * @LastEditors: Akko
+ * @Description: 主函数
+ * @FilePath: /EIS/main.cpp
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+#include <iostream>
+#include "Employee.h"
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Employee;
+
+int Employee::Employee_Number = 0;
+
